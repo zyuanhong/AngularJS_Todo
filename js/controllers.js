08ca0ab9240ebdf1e4fd93a099e39fefccbcdac0
Yuan-Hong Zeng <zmetahong@gmail.com>
2013-07-10 17:40:22 +0800

    Complete add Todo List

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,10 @@
 function TodoCrtl($scope) {
   $scope.newItem = "";
   $scope.todoList = [{ label: "buy milk" }, { label: "pay power cost" }];
+  $scope.addItem = function() {
+    if(this.newItem) {
+      this.todoList.push({label:this.newItem});
+      this.newItem = "";
+    }
+  }
 }
